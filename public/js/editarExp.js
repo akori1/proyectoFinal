763feacd7eb8a7ea524a67d37e299fcf83b1cf61
fga8 <fga_8@hotmail.com>
2016-06-14 16:08:06 -0300

    editar experiencia

--- public/js/editarExp.js
+++ public/js/editarExp.js
@@ -1,15 +1,35 @@
 var dir = location.search;
 var dividir = dir.split("=");
 var id = dividir[1];
+var url= 'http://connectedin.herokuapp.com/person/%id%';
+        url = url.replace (/%id%/g,id);
+var algo=[];
+var i=-1;
 
 function botonAgregar () {
+    traer_Datos(id);
+    
     $('#button')
     .on('click', agregarExp);
 
+    function traer_Datos(id){
+        $.get({
+            url:url,
+            data:{       
+             },
+         success:function(data){
+           // algo= data;
+           // console.log('metodo get',algo);
+           
+
+          }  
+
+        });
+    }
+
     function agregarExp(e){ 
         e.preventDefault ();
-   
-     var experience=[
+     var exp=
         {
             fotoTrabajo:$('.fotoTrabajo').val(),
             lugarTrabajo:$('.lugarTrabajo').val(),
@@ -17,33 +37,48 @@ function botonAgregar () {
             fechaFinTrabajo:$('.fechaFinTrabajo').val(),
             cargoTrabajo:$('.cargoTrabajo').val(),
             infoTrabajo:$('.infoTrabajo').val(),
-        }];
-        var url= 'http://connectedin.herokuapp.com/person/%id%';
-        url = url.replace (/%id%/g,id);
-         
-         $.ajax({
-    url:url,
-    method:'PUT',
-    contentType: "application/json",
-    data : JSON.stringify(
-             experience
-             ),
-   success: function(data){ 
-            alert('Experiencia Agregada'); 
-            console.log('CARGO EXPERIENCIA:');
-            console.log(experiencia);
-        },
-   error: function(){
-            alert('Faltan ingresar campos');
+        };
+        i++;
+       algo[i]=exp;
+        console.log('imprime antes del put',algo[i]);
+     
+
+      var registro = {
+            firstName : $('.firstName').val(),
+            lastName : $('.lastName').val(),                     
+            address : $('.address').val(),
+            photo : $('.photo').val(),
+            email : $('.email').val(),
+            education:$('.education').val(),
+            experience:algo
         }
-  }); 
+        
+
+        
+        
+         $.ajax({
+            url:url,
+            method:'PUT',
+            contentType: "application/json",
+            data : JSON.stringify(registro
+                     ),
+           success: function(data){ 
+                    alert('Experiencia Agregada'); 
+                    console.log('CARGO EXPERIENCIA:');
+                    console.log(exp);
+                  //  console.log(data);
+                },
+           error: function(){
+                    alert('Faltan ingresar campos');
+                }
+          }); 
         
     }
 };/*
 function obtengoFormulario()
 {
 
-	var url = 'http://connectedin.herokuapp.com/person/%id%';
+ var url = 'http://connectedin.herokuapp.com/person/%id%';
         url = url.replace (/%id%/g,id);
         console.log (url);
       $.get(url,function(data){
