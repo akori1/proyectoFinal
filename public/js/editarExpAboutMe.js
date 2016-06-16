300f3f7dbc4008608183ab39de7c40c2c62c31e3
AgustinProgramador <Augusrinprogramador@hotmail.com.ar>
2016-06-16 11:39:57 -0300

    final

--- public/js/editarExpAboutMe.js
+++ public/js/editarExpAboutMe.js
@@ -3,61 +3,91 @@ var params = [];
 var dir = location.search.substr(1, location.search.length).split('&');
 
 for (var i = 0; i < dir.length; i++) {
-	var tmpParam = dir[i].split('=');
-	params[tmpParam[0]] = tmpParam[1];
+    var tmpParam = dir[i].split('=');
+    params[tmpParam[0]] = tmpParam[1];
 }
 /* ENCUENTRO EL ID Y LA POS */
 var id = params['id'];
 var pos = params['pos'];
-
-function editarExperiencia(){
-	$('#button')
-    .on('click', reemplazarDatos);
-		
-		function traerDatos(id)
-		{
-		var url = 'http://connectedin.herokuapp.com/person/%id%';
+        var url = 'http://connectedin.herokuapp.com/person/%id%';
         url = url.replace (/%id%/g,id);
-		$.get({
-			url:url,
-			data:{},
-			success:function(data){
-			console.log(data);   
-    	$('.fotoTrabajo').val(data.experience[pos].fotoTrabajo);
-     	$('.lugarTrabajo').val(data.experience[pos].lugarTrabajo);
-     	$('.fechaInicioTrabajo').val(data.experience[pos].fechaInicioTrabajo);
-     	$('.fechaFinTrabajo').val(data.experience[pos].fechaFinTrabajo);
-     	$('.cargoTrabajo').val(data.experience[pos].cargoTrabajo);
-     	$('.infoTrabajo').val(data.experience[pos].infoTrabajo);
+var array=[];
+function editarExperiencia(e){
+    traerDatos(id); 
+    traerRegistro(id);  
+    $('#button')
+    .on('click', reemplazarDatos);
+        function traerDatos(id)
+        {
+        $.get({
+            url:url,
+            data:{},
+            success:function(data){
+            console.log(data);   
+        $('#fotoTrabajo').val(data.experience[pos].fotoTrabajo);
+        $('.lugarTrabajo').val(data.experience[pos].lugarTrabajo);
+        $('.fechaInicioTrabajo').val(data.experience[pos].fechaInicioTrabajo);
+        $('.fechaFinTrabajo').val(data.experience[pos].fechaFinTrabajo);
+        $('.cargoTrabajo').val(data.experience[pos].cargoTrabajo);
+        $('.infoTrabajo').val(data.experience[pos].infoTrabajo);
+        
    }
-  	 });
-		}
-		   var exp=
-       		 {
-            fotoTrabajo:$('.fotoTrabajo').val(),
+     });
+        }
+        function traerRegistro(id)
+        {
+            $.get({
+                url:url,
+                data:{},
+                success:function(data)
+                {
+                    array=data.experience;
+                    console.log(array);
+                }
+            })
+
+
+        }            
+        function reemplazarDatos(e)
+        {
+        
+    e.preventDefault();
+        
+           var exp=
+             {
+            fotoTrabajo:$('#fotoTrabajo').val(),
             lugarTrabajo:$('.lugarTrabajo').val(),
             fechaInicioTrabajo:$('.fechaInicioTrabajo').val(),
             fechaFinTrabajo:$('.fechaFinTrabajo').val(),
             cargoTrabajo:$('.cargoTrabajo').val(),
             infoTrabajo:$('.infoTrabajo').val(),
              };
-        	data.experience[pos]=exp;
-        function reemplazarDatos(data)
-        {
-        
+             array[pos]=exp;
+             console.log(exp);
+     var registro = {
+            firstName : $('.firstName').val(),
+            lastName : $('.lastName').val(),                     
+            address : $('.address').val(),
+            photo : $('.photo').val(),
+            email : $('.email').val(),
+            education:$('.education').val(),
+            summary:$('.summary').val(), 
+            experience:array,
+        }
+       // registro.experience[pos]=exp;
         $.ajax({
             url:url,
             method:'PUT',
             contentType:"application/json",
-            data: JSON.stringify(data),
+            data: JSON.stringify(registro),
             success: function(data)
-        	{
-
-        	}
+            {
+                    alert('EXPERIENCIA MODIFICADA');
+            }
         });
-	}
+    }
 
-traerDatos(id);
-reemplazarDatos();
+//reemplazarDatos();
 }
+//traerDatos(id);
 editarExperiencia();
